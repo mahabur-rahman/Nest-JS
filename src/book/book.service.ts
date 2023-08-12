dcacc7bd0b7e0f6dc97031db97db3860b8a0f084
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-08-12 16:24:29 +0600

    validation of books

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './schemas/book.schema';
 import * as mongoose from 'mongoose';
@@ -25,6 +29,11 @@ export class BookService {
 
   //   get single book from param
   async findBook(id: string): Promise<Book> {
+    const isValidId = mongoose.isValidObjectId(id);
+    // console.log(isValidId);
+    if (!isValidId) {
+      throw new BadRequestException('Please enter correct id!');
+    }
     const book = await this.bookModel.findById(id);
     if (!book) {
       throw new NotFoundException('Book not found!');
--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,9 +1,24 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { Category } from '../schemas/book.schema';
 
 export class UpdateBookDto {
+  @IsString()
+  @IsOptional()
   readonly title: string;
+
+  @IsString()
+  @IsOptional()
   readonly description: string;
+
+  @IsString()
+  @IsOptional()
   readonly author: string;
+
+  @IsString()
+  @IsOptional()
   readonly price: number;
+
+  @IsOptional()
+  @IsEnum(Category, { message: 'enter correct category!' })
   readonly category: Category;
 }
